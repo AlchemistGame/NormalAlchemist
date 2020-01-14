1fd5966d3ffdacdca99943f137d63e7793971159
jnereus <281804140@qq.com>
2020-01-15 01:06:34 +0800

    refs #5 尝试加入配表序列化，试试看用protobuf

---
+++ Tools/ConfigLoad/ConfigLoad/ProtoGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace ConfigLoad
+{
+    class ProtoGeneration
+    {
+
+
+        public static string RunProtocEXE(string stringcommandLine, int packagesize)
+        {
+            Process process = new Process();
+            process.StartInfo.Arguments = stringcommandLine;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.FileName = "ping.exe";
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+            return process.StandardOutput.ReadToEnd();//返回结果
+        }
+
+    }
+}
